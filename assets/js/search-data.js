13c9a78cc37c1ae2213ca304ed0ce89760cf372f
yicheng-tong <e1538247@u.nus.edu>
2025-10-11 04:29:31 +0000

    Deploying to gh-pages from @ yicheng-tong/yicheng-tong.github.io@f31c2ed8fcefc6f2cf00e38212c477215df317e8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "",
+          description: "Download my CV in PDF format",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
